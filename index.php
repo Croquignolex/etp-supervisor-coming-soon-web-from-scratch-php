29b830234eef3f1d8b4bfbb22afac4cc4ac2d01c
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2020-12-08 13:32:04 +0100

    Remaining project files

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Alex NGOMBOL,Steve NGANSO">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="MMAC" />
+        <meta name="keywords" content="MMAC" />
+
+        <meta name="robots" content="noindex">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="base-url" content="https://etp-admin.moyo-industry.com/">
+        <meta name="locale" content="fr">
+
+        <title>Admin MMAC</title>
+
+        <link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192" href="img/favicon/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="img/favicon/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+<!--        <link rel="manifest" href="img/favicon/manifest.json">-->
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="img/favicon/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+
+        <meta name="apple-mobile-web-app-title" content="Alex NGOMBOL,Steve NGANSO">
+        <meta name="application-name" content="Alex NGOMBOL,Steve NGANSO">
+
+        <meta property="og:title" content="Alex NGOMBOL,Steve NGANSO" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="fr" />
+        <meta property="og:ttl" content="0" />
+
+        <meta property="og:description" content="MMAC">
+        <meta property="og:url" content="https://etp-admin.moyo-industry.com/" />
+        <meta property="og:image" content="img/logo.png" />
+        <meta property="twitter:image" content="img/logo.png" />
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/responsive.css" rel="stylesheet">
+
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    </head>
+
+    <body class="snow-bg">
+
+        <canvas id="snow"></canvas>
+
+        <div class="main">
+            <header class="page-header">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-5 col-sm-12 mt-30">
+                            <div class="page-logo col-sm-12"><img src="img/logo.png" alt="logo" class="logo"></div>
+                            <div class="countdown-content countdown-text col-sm-12">
+                                <h1>Bientôt les <span>admin</span> auront le controle...</h1>
+                                <p>
+                                    L'administrateur d'une applicatin web est en charge des activités de suivi
+                                    d'exploitation (gestion des traitements et des données) liées à celle-ci.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="countdown col-md-7 col-sm-12">
+                            <div class="time">
+                                <div id="count">
+                                    <div id="dayTimer">
+                                        <span class="days"></span>
+                                        <div class="count-title" id="day_title"></div>
+                                    </div>
+                                    <div id="hourTimer">
+                                        <span class="hours"></span>
+                                        <div class="count-title" id="hour_title"></div>
+                                    </div>
+                                    <div id="minuteTimer">
+                                        <span class="minutes"></span>
+                                        <div class="count-title" id="minute_title"></div>
+                                    </div>
+                                    <div id="secondTimer">
+                                        <span class="seconds"></span>
+                                        <div class="count-title" id="second_title"></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="buttons col-sm-12">
+                                <a class="custom-btn toggle-more" href="https://etp.moyo-industry.com/">Je me suis trompé de rôle</a>
+                            </div>
+
+                            <div class="social-media col-sm-12">
+                                <a href="javascript:void(0)"><i class="fab fa-twitter"></i></a>
+                                <a href="javascript:void(0)"><i class="fab fa-facebook-f"></i></a>
+                                <a href="javascript:void(0)"><i class="fab fa-google-plus"></i></a>
+                                <a href="javascript:void(0)"><i class="fab fa-instagram"></i></a>
+                                <a href="javascript:void(0)"><i class="fab fa-linkedin"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </div>
+
+        <script src="js/jquery.min.js"></script>
+        <script src="js/fontawesome-all.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/snow.js"></script>
+        <script src="js/countdown.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
